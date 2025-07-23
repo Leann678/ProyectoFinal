a7a052156f4e2c338d7f5415a9469a4fa0470219
Leann678 <LYDR0001@ce.pucmm.edu.do>
2025-07-23 19:26:38 -0400

    Update Diseñador.java
    agregacion de metodos basicos

--- gestionSoftware/src/logico/Diseñador.java
+++ gestionSoftware/src/logico/Diseñador.java
@@ -1,13 +1,16 @@
 package logico;
 
-public class Diseñador extends Trabajador{
+public class DiseÃ±ador extends Trabajador{
 
-	public Diseñador(String cedulaTrab, String nombre, String apellido, String direccion, String genero, int edad,
+	public DiseÃ±ador(String cedulaTrab, String nombre, String apellido, String direccion, String genero, int edad,
 			float salario, String proyecto, String calificacionAnual) {
 		super(cedulaTrab, nombre, apellido, direccion, genero, edad, salario, proyecto, calificacionAnual);
 		// TODO Auto-generated constructor stub
 	}
 
-	
+		public boolean esDestacado() {
+	    return calificacionAnual.equalsIgnoreCase("Destacado");
+	}
+
 
 }
