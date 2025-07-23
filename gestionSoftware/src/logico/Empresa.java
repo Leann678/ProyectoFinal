ad5f9c2e49fc8dec7b5ee68e2c681439d831646d
Leann678 <LYDR0001@ce.pucmm.edu.do>
2025-07-23 19:30:39 -0400

    Update Empresa.java
    declaracion de las variables y agregacion del metodo registrar trabajador

--- gestionSoftware/src/logico/Empresa.java
+++ gestionSoftware/src/logico/Empresa.java
@@ -1,6 +1,23 @@
 package logico;
 
-public class Empresa {
-	
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-}
+public class Empresa {
+    private List<Trabajador> misTrab;
+    private List<Cliente> misClientes;
+    private List<Contrato> contratos;
+    private List<Proyecto> proyectos;
+
+    public Empresa() {
+        misTrab = new ArrayList<>();
+        misClientes = new ArrayList<>();
+        contratos = new ArrayList<>();
+        proyectos = new ArrayList<>();
+    }
+
+    public boolean registrarTrabajador(Trabajador t) {
+        return misTrab.add(t);
+    }
