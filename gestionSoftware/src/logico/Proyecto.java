9983dd342ff649823a336c8c58cb8079c940f8d4
Leann678 <LYDR0001@ce.pucmm.edu.do>
2025-07-23 20:02:18 -0400

    Update Proyecto.java
    agregacion de los metodos basicos

--- gestionSoftware/src/logico/Proyecto.java
+++ gestionSoftware/src/logico/Proyecto.java
@@ -1,34 +1,55 @@
 package logico;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Proyecto {
 	private String nombre;
-	private List<Trabajador>misTrab;
-	private List<Cliente>misClientes;
-	public Proyecto(String nombre, List<Trabajador> misTrab, List<Cliente> misClientes) {
+	private ArrayList<Trabajador>misTrab;
+	private ArrayList<Cliente>misClientes;
+	public Proyecto(String nombre, ArrayList<Trabajador> misTrab, ArrayList<Cliente> misClientes) {
 		super();
 		this.nombre = nombre;
-		this.misTrab = misTrab;
-		this.misClientes = misClientes;
-	}
+		this.misTrab = new ArrayList<>();
+	    this.misClientes = new ArrayList<>();	}
 	public String getNombre() {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public List<Trabajador> getMisTrab() {
+	public ArrayList<Trabajador> getMisTrab() {
 		return misTrab;
 	}
-	public void setMisTrab(List<Trabajador> misTrab) {
+	public void setMisTrab(ArrayList<Trabajador> misTrab) {
 		this.misTrab = misTrab;
 	}
 	public List<Cliente> getMisClientes() {
 		return misClientes;
 	}
-	public void setMisClientes(List<Cliente> misClientes) {
+	public void setMisClientes(ArrayList<Cliente> misClientes) {
 		this.misClientes = misClientes;
+	}
+	
+	public boolean agregarCliente(Cliente cliente) {
+	    if (misClientes.size() >= 5) return false;
+	    return misClientes.add(cliente);
+	}
+
+	public boolean estaSaturado() {
+	    return misClientes.size() >= 5;
+	}
+
+	public void agregarTrabajador(Trabajador trabajador) {
+		misTrab.add(trabajador);
+	}
+
+	public ArrayList<Trabajador> obtenerTrabajadoresPorRol(String rol) {
+	    return misTrab.stream()
+	        .filter(t -> t.getClass().getSimpleName().equalsIgnoreCase(rol))
+	        .collect(Collectors.toCollection(ArrayList::new));
+
 	}
 
 }
