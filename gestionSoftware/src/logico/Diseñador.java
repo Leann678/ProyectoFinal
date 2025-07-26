b933b9f04b2bc36c17d688a9ceb19f7f34cc65a6
Leann678 <LYDR0001@ce.pucmm.edu.do>
2025-07-26 18:26:04 -0400

    Update Diseñador.java
    Agregacion de variable proyectoAsignado

--- gestionSoftware/src/logico/Diseñador.java
+++ gestionSoftware/src/logico/Diseñador.java
@@ -1,10 +1,10 @@
 package logico;
 
-public class Diseñador extends Trabajador{
+public class DiseÃ±ador extends Trabajador{
 
-	public Diseñador(String cedulaTrab, String nombre, String apellido, String direccion, String genero, int edad,
-			float salario, String proyecto, String calificacionAnual) {
-		super(cedulaTrab, nombre, apellido, direccion, genero, edad, salario, proyecto, calificacionAnual);
+	public DiseÃ±ador(String cedulaTrab, String nombre, String apellido, String direccion, String genero, int edad,
+			float salario, String proyecto, String calificacionAnual, Proyecto proyectoAsignado) {
+		super(cedulaTrab, nombre, apellido, direccion, genero, edad, salario, proyecto, calificacionAnual, proyectoAsignado);
 		// TODO Auto-generated constructor stub
 	}
 
